245f45778f6922d34f03ac5e8d35f96973eecc1a
AnderGalisteo <andergalisteo@gmail.com>
2021-10-14 14:08:39 +0200

    Update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,11 +12,11 @@ const Footer = () => (
   >
     <div className="container">
       <p>
-        A GatsbyJS Starter for Netlify CMS, Made with{" "}
+        A blog about what I find interesting made with{" "}
         <span className="icon -love">
           <RiHeart2Line />
         </span>{" "}
-        by <Link to="/">Stackrole.com</Link>
+        by <Link to="www.andergalisteo.com">Ander Galisteo</Link>
       </p>
     </div>
   </footer>
